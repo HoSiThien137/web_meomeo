790cf5ecaf9b3c131be848593a21f938c16f7421
HoSiThien137 <4701104199@student.hcmue.edu.vn>
2024-12-05 01:26:43 +0700

    fix header

--- tiktok-ui/src/components/Popper/Menu/index.js
+++ tiktok-ui/src/components/Popper/Menu/index.js
@@ -44,7 +44,7 @@ function Menu({children, items = []},hideOnClick=false, onChange=defaultFn) {
                           }}
                         /> 
                       )} 
-                      {renderItems()}
+                      <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                   </div>
           
